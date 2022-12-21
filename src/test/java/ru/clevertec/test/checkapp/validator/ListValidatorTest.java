628b4d610aab5e00349c45987659a67bb36c56be
Dayanch(PC) <ishankulievdayantch@yandex.ru>
2022-12-21 21:08:25 +0300

    add test for ListValidatorTest and ProductValidatorTest

--- src/test/java/ru/clevertec/test/checkapp/validator/ListValidatorTest.java
+++ src/test/java/ru/clevertec/test/checkapp/validator/ListValidatorTest.java
@@ -1,12 +1,21 @@
 package ru.clevertec.test.checkapp.validator;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ListValidatorTest {
 
     @Test
-    void isListEmpty() {
+    void isListEmptyShouldBeTrue() {
+        Assertions.assertTrue(ListValidator.isListEmpty(List.of()));
+    }
+
+    @Test
+    void isListEmptyShouldBeFalse() {
+        Assertions.assertFalse(ListValidator.isListEmpty(List.of(1)));
     }
 }
