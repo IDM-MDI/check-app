b08ddfe20b65b103d21227e344bc04a68e265363
Lenovo Laptop <ishankulievdayantch@yandex.ru>
2023-03-02 03:34:11 +0300

    edit RequestParameterUtilTest

--- src/test/java/ru/clevertec/test/checkapp/util/RequestParameterUtilTest.java
+++ src/test/java/ru/clevertec/test/checkapp/util/RequestParameterUtilTest.java
@@ -1,6 +1,6 @@
 package ru.clevertec.test.checkapp.util;
 
-import org.junit.jupiter.api.Assertions;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -10,32 +10,39 @@ class RequestParameterUtilTest {
     static final String[] TEST_ARRAY = List.of("id=1","id=2","id=3","card=1234")
             .toArray(new String[0]);
     @Test
-    void findProducts() {
+    void findProductsShouldReturnCorrectValue() {
         List<Long> expected = List.of(1L,2L,3L);
         List<Long> actual = RequestParameterUtil.findProducts(TEST_QUERY);
-        Assertions.assertEquals(expected,actual);
+        Assertions.assertThat(actual).containsAll(expected);
     }
 
     @Test
-    void separateQuery() {
+    void separateQueryShouldReturnCorrectValue() {
         String[] actual = RequestParameterUtil
                 .separateQuery(TEST_QUERY);
-        Assertions.assertArrayEquals(TEST_ARRAY,actual);
+        Assertions.assertThat(actual).containsExactly(TEST_ARRAY);
     }
 
     @Test
-    void findQueryValue() {
+    void findQueryValueShouldReturnCorrectValue() {
         String expected = "1234";
         String[] strings = RequestParameterUtil
                 .separateQuery(TEST_QUERY);
         String[] values = RequestParameterUtil.findQueryValue("card", strings);
-        Assertions.assertEquals(expected, values[0]);
+        Assertions.assertThat(values[0]).isEqualTo(expected);
     }
 
     @Test
-    void findDiscountCardNumber() {
+    void findDiscountCardNumberShouldReturnCorrectValue() {
         int expected = 1234;
         int actual = RequestParameterUtil.findDiscountCardNumber(TEST_QUERY);
-        Assertions.assertEquals(expected,actual);
+        Assertions.assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    void findDiscountCardNumberShouldReturnZero() {
+        int expected = 0;
+        int actual = RequestParameterUtil.findDiscountCardNumber("");
+        Assertions.assertThat(actual).isEqualTo(expected);
     }
 }
